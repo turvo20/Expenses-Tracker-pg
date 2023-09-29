10c47aa162cb20efdb5c2f5da83ebd4572c10751
Rudy Casilla <turvo200@gmail.com>
2023-09-29 16:46:14 -0400

    probando auth

--- src/routers/Auth.router.js
+++ src/routers/Auth.router.js
@@ -1,8 +1,8 @@
 import { Router } from 'express'
 
-// import { validarCampos } from '../Middleware/index.js'
-// import { check } from 'express-validator'
-// import { Confirmar, LoginUser, createUser } from '../modules/auth/auth.controller.js'
+import { validarCampos } from '../Middleware/index.js'
+import { check } from 'express-validator'
+import { Confirmar, LoginUser, createUser } from '../modules/auth/auth.controller.js'
 
 
 
@@ -17,14 +17,14 @@ authRouter.get('/',(req,res)=>{
 //     check('password', 'El password debe de ser de 6 caracteres').isLength({ min: 6 }),
 //     validarCampos
 // ], LoginUser)
-// authRouter.post('/sing-up',
-// [ // middlewares
-//         check('fullname', 'El nombre es obligatorio').not().isEmpty(),
-//         check('username', 'El nombre de usuario es obligatorio').not().isEmpty(),
-//         check('email', 'El email es obligatorio').isEmail(),
-//         check('password', 'El password debe de ser de 6 caracteres').isLength({ min: 6 }),
-//         validarCampos
-// ],createUser)
+authRouter.post('/sing-up',
+[ // middlewares
+        check('fullname', 'El nombre es obligatorio').not().isEmpty(),
+        check('username', 'El nombre de usuario es obligatorio').not().isEmpty(),
+        check('email', 'El email es obligatorio').isEmail(),
+        check('password', 'El password debe de ser de 6 caracteres').isLength({ min: 6 }),
+        validarCampos
+],createUser)
 
 // authRouter.get('/confirmed/:token', Confirmar)
 
