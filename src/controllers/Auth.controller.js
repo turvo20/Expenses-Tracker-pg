5d9f1f7773d039ee3d9ebe004ef227f595972478
Rudy Casilla <turvo200@gmail.com>
2023-10-16 23:13:35 -0400

    pwd5

--- src/controllers/Auth.controller.js
+++ src/controllers/Auth.controller.js
@@ -94,7 +94,7 @@ export const Confirmar = async (req, res) =>{
  export const actualizarPassword = async (req, res) => {
  
     try {
-        const resp = await AuthServices.actualizarPassword(...req.body);
+        const resp = await AuthServices.actualizarPassword(req.body);
         res.status(200).json(resp)
     } catch (error) {
         console.log(error)
